26a8d50d14ddd2ebf322a8cc185f08d6be46d713
Stefano <stocastico@gmail.com>
2022-11-25 14:06:25 +0100

    Working version of the function getting coordinates

--- get_coords.js
+++ get_coords.js
@@ -1,22 +1,46 @@
-var url = "https://en.wikipedia.org/w/api.php"; 
+function fill_with_lat_lon() {
+    console.log("Fill with lat lon - Table");
+    var table = document.getElementById('table-body');
+    if (table) {
+        console.log("Processing table elements")
+        console.log("There are " + table.rows.length + " rows")
+        for (let row of table.rows) 
+        {
+            let location = row.cells[0].innerText
+            console.log("Current location: " + location);
 
-var params = {
-    action: "query",
-    prop: "coordinates",
-    titles: "Wikimedia Foundation",
-    format: "json"
-};
+            var url = "https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=" + location + "&format=json&origin=*"; 
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+            const getJsonData = async() => {
+                let data = await fetch(url).then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok')
+                    }
+                    return response.json();
+                })
+                .then((json_) => {
+                    //console.log("json: ", json_)
+                    return json_.query.pages;
+                })
+                .catch((error) => {
+                    console.error('There has been a problem with your fecth operation:', error);
+                })
+                return data;
+            }
+            
+            const getData = async () => {
+                let pages = await getJsonData()
+                //console.log("data: ", pages);
+                var page = pages[Object.keys(pages)[0]];
+                var lat = page.coordinates[0].lat
+                var lon = page.coordinates[0].lon
+                console.log("Latitute: " + lat);
+                console.log("Longitude: " + lon);
+                row.cells[1].innerText = lat;
+                row.cells[2].innerText = lon;
+            }
 
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {
-        var pages = response.query.pages;
-        for (var page in pages) {
-            console.log("Latitute: " + pages[page].coordinates[0].lat);
-            console.log("Longitude: " + pages[page].coordinates[0].lon);
+            getData();
         }
-    })
-    .catch(function(error){console.log(error);});
+    }
+  }
