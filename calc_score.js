33342e6a90e9e8d40fe1f361d96a472afd61f9b3
Stefano <stocastico@gmail.com>
2022-11-29 13:28:24 +0100

    complete scoring function

--- calc_score.js
+++ calc_score.js
@@ -8,15 +8,39 @@ function score(lat, lon, lat_correcta, lon_correcta, dificultad, time_left)
     const CLOSE_FOR_BONUS = 300; // se puede tunear quizás
     const TIME_FOR_BONUS = 20;
     const MAX_POINTS = 10;
-    var result = 0;
-    result += dificultad/2;
+    var points = 0;
+    points += dificultad/2;
     const d = distance(lat, lon, lat_correcta, lon_correcta);
     if (d <= CLOSE_FOR_BONUS && time_left >= TIME_FOR_BONUS)
     {
-        result += 1;
+        points += 1;
     }
-    const max_points_left = 10 - result; // assign remaining points based on the distance from the correct answer
+    const max_points_left = 10 - points; // assign remaining points based on the distance from the correct answer
 
-    
+    if (distance < 50) {
+        points += max_points_left;
+    }
+    else if (distance < 100) {
+        points += 0.9 * max_points_left;
+    }
+    else if (distance < 200) {
+        points += 0.8 * max_points_left;
+    }
+    else if (distance < 300) {
+        points += 0.7 * max_points_left;
+    }
+    else if (distance < 400) {
+        points += 0.6 * max_points_left;
+    }
+    else if (distance < 500) {
+        points += 0.5 * max_points_left;
+    }
+    else if (distance < 1000) {
+        points += 0.25 * max_points_left;
+    }
+    else if (distance < 2000) {
+        points += 0.1 * max_points_left;
+    }
 
+    return points;
 }
